d0c2b0292d85eae2853d1ae52361dee00a4acbe0
RodrigoSantosgit <56035598+RodrigoSantosgit@users.noreply.github.com>
2020-04-21 12:51:55 +0100

    views update

--- serverDiff/room_displayer_app/views.py
+++ serverDiff/room_displayer_app/views.py
@@ -32,30 +32,19 @@ def departments_book(request):
 ######################################################################################
 
 def depart_book_rooms(request, dep_id):
-    #depart = get_object_or_404(Building, id=dep_id)
-
-    #rooms = get_list_or_404(Room, building_id=dep_id)
+    
     depart = (4,'DETI')
     context = {'depart':depart, 'rooms1': [], 'rooms2': [], 'rooms3': []}
     mycursor.execute("SELECT * FROM Room WHERE building_id = 4")
 
     myresult = mycursor.fetchall()
     for r in myresult:
-    #    r.name = r.name.upper()
-    #    if not (r.name[0].isdigit()):
-    #        context['rooms1'] = context['rooms1'] + [r]
-    #    elif int(r.name.split('.')[1]) == 1:
-    #        context['rooms1'] = context['rooms1'] + [r]
-    #    elif int(r.name.split('.')[1]) == 2:
-    #        context['rooms2'] = context['rooms2'] + [r]
-    #    elif int(r.name.split('.')[1]) == 3:
-    #        context['rooms3'] = context['rooms3'] + [r]
-            if int(r[1].split('.')[1]) == 1:
-                context['rooms1'] = context['rooms1'] + [r]
-            elif int(r[1].split('.')[1]) == 2:
-                context['rooms2'] = context['rooms2'] + [r]
-            elif int(r[1].split('.')[1]) == 3:
-                context['rooms3'] = context['rooms3'] + [r] 
+        if int(r[1].split('.')[1]) == 1:
+            context['rooms1'] = context['rooms1'] + [r]
+        elif int(r[1].split('.')[1]) == 2:
+            context['rooms2'] = context['rooms2'] + [r]
+        elif int(r[1].split('.')[1]) == 3:
+            context['rooms3'] = context['rooms3'] + [r] 
 
     
     return render(request, 'departamento_reserva_salas.html', context)
@@ -63,8 +52,7 @@ def depart_book_rooms(request, dep_id):
 ######################################################################################
 
 def room_book_timetable(request, dep_id, room_id):
-    #get_events = get_list_or_404(Event, room_id=room_id)
-	
+
     slQ = "SELECT * FROM Event WHERE room_id = %(id)s"
     mycursor.execute(slQ, { 'id': room_id })
 
@@ -84,13 +72,13 @@ def room_book_timetable(request, dep_id, room_id):
 
 @csrf_exempt
 def book(request):
-    #dep_id = Building.objects.get(id=request.POST['dep_id'])
+
     slQ = "SELECT * FROM Building WHERE id = %(id)s"
     mycursor.execute(slQ, { 'id': request.POST.get('dep_id') })
 
     dep = mycursor.fetchall()
 
-    #room_id =Room.objects.get(id=request.POST['room_id'])
+
     slQ = "SELECT * FROM Room WHERE id = %(id)s"
     mycursor.execute(slQ, { 'id': request.POST.get('room_id') })
 
@@ -111,8 +99,6 @@ def book(request):
 
 def salas(request, dep_id):
 
-    #rooms = get_list_or_404(Room, building_id=dep_id)
-
     time = datetime.now()
     salas = {'rooms1' : [], 'rooms2' : [], 'rooms3' : []}
     
@@ -123,9 +109,6 @@ def salas(request, dep_id):
 
     for r in myresult:
         if check_room_event(r[0], time):
-    #        r.name = r.name.upper()
-    #        if not (r.name[0].isdigit()):
-    #            salas['rooms1'] = salas['rooms1'] + [r]
             if int(r[1].split('.')[1]) == 1:
                 salas['rooms1'] = salas['rooms1'] + [r]
             elif int(r[1].split('.')[1]) == 2:
@@ -142,21 +125,20 @@ def horario_v2(request, dep_id, room_id):
     mycursor.execute(slQ, { 'id': room_id })
 
     room = mycursor.fetchall()
-    #sala_name = get_object_or_404(Room, id=room_id).name
+
     room_name = room[0][1]
 	
     slQ = "SELECT * FROM Event WHERE room_id = %(id)s"
     mycursor.execute(slQ, { 'id': room_id })
 
     get_events = mycursor.fetchall()
-    #get_events = get_list_or_404(Event, room_id=room_id)
-    #room_name = Room.objects.get(id=room_id).name.upper()
+
     context = {
-	    'events' : get_events,
-            'dep_id': dep_id,
-            'room_id': room_id,
-            'room_name': room_name,
-	}
+        'events' : get_events,
+        'dep_id': dep_id,
+        'room_id': room_id,
+        'room_name': room_name,
+    }
     return render(request, 'horario_v2.html', context=context)
 
 ######################################################################################
@@ -169,8 +151,6 @@ def horario_v2(request, dep_id, room_id):
 
 def check_room_event(rid, time):
 
-    #events = list(Event.objects.filter(room_id=rid))
-    
     slQ = "SELECT * FROM Event WHERE room_id = %(id)s"
     mycursor.execute(slQ, { 'id': rid })
 
@@ -180,17 +160,6 @@ def check_room_event(rid, time):
 
     if len(events) == 0:
         return True
-
-    #for e in events:
-    #    sd = e.Start_date
-    #    ed = e.End_date
-    #   if (int(sd.strftime("%d")) == int(time.strftime("%d"))) and (int(sd.strftime("%m")) == int(time.strftime("%m"))) and (int(sd.strftime("%Y")) == int(time.strftime("%Y"))):
-    #        if int(sd.strftime("%H")) <= hora:
-    #            if int(ed.strftime("%H")) > hora:
-    #                return False
-    #            elif int(ed.strftime("%H")) == hora:
-    #                if int(ed.strftime("%M")) > int(time.strftime("%M")):
-    #                    return False
 
     for e in events:
         sd = e[2]
